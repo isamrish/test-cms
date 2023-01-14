65909385907f1c5f14f0e3e716773542c6c6f65c
isamrish <askmaurya48@gmail.com>
2023-01-14 23:54:09 +0530

    update
    Signed-off-by: isamrish <askmaurya48@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -69,6 +69,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/products">
                 Products
               </Link>
+              <Link className="navbar-item" to="/hello-world">
+                Hello World
+              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
---
+++ src/pages/hello-world.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloWorldPage = () => {
+  return <div>Hello world page</div>;
+};
+
+export default HelloWorldPage;
